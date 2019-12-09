c765334758d82001740f0ad83fcea62489df47d5
Andrew Louis Colon <21088@columbushs.com>
2019-12-09 11:15:29 -0500

    commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,37 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(Damage) {
+        this.health = this.health - Damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+    receiveDamage(Damage) {
+        this.health = this.health - Damage;
+        if (Viking.health > 0) {
+            return name + "has received" + Damage + "points of damage"
+        }
+        else if(Viking.health < 0) {
+            return "NAME has died in act of combat";
+        }
+        
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
